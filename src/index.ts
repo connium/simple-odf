714c16fd854fade1bdb69debf3213c83d49b3d36
Christoph Nölke <post@christoph-noelke.de>
2018-01-10 20:09:34 +0100

    remove index.ts from namespaces, export API from main index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export { TextDocument } from "./TextDocument";
+
+// style
+export { HorizontalAlignment } from "./style/HorizontalAlignment";
+
+// text
+export { Headline } from "./text/Headline";
+export { Paragraph } from "./text/Paragraph";
