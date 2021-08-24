33f8728c5e72bd24b43e6962a1857b4f623650f0
Christoph Nölke <connium@users.noreply.github.com>
2021-08-24 17:10:01 +0200

    refactor: no any-typed parameters (#537)
    Change any-typed parameters of utility functions to be of type unknown.

--- src/api/util/isPositiveLength.ts
+++ src/api/util/isPositiveLength.ts
@@ -3,11 +3,10 @@
  *
  * ([0-9]*[1-9][0-9]*(\.[0-9]*)?|0+\.[0-9]*[1-9][0-9]*|\.[0-9]*[1-9][0-9]*)((cm)|(mm)|(in)|(pt)|(pc)|(px))
  *
- * @param {any} value The value that is to be checked
+ * @param {unknown} value The value that is to be checked
  * @returns {boolean} `true` if the given value is a positive number, `false` otherwise
  * @private
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isPositiveLength(value: any): boolean {
+export function isPositiveLength(value: unknown): boolean {
   return typeof value === 'number' && value >= 0;
 }
